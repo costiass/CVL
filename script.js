36c2673d147dc69ce416638ed52d6db145a05597
costiass <costiass.pro@gmail.com>
2025-02-15 17:27:39 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById('btn').addEventListener('click', function() {
+    const elem = document.getElementById('body');
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) {
+        elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) {
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) {
+        elem.msRequestFullscreen();
+    }
+});
