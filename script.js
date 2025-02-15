b7501c625688bd5d15c42041007bcd0b3bcfb0ba
costiass <costiass.pro@gmail.com>
2025-02-15 17:31:49 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-document.getElementById('btn').addEventListener('click', function() {
-    const elem = document.getElementById('body');
-    if (elem.requestFullscreen) {
-        elem.requestFullscreen();
-    } else if (elem.mozRequestFullScreen) {
-        elem.mozRequestFullScreen();
-    } else if (elem.webkitRequestFullscreen) {
-        elem.webkitRequestFullscreen();
-    } else if (elem.msRequestFullscreen) {
-        elem.msRequestFullscreen();
+document.addEventListener("DOMContentLoaded", function() {
+    const sections = document.querySelectorAll("section");
+    let currentSection = 0;
+
+    function showSection(index) {
+        sections.forEach((section, i) => {
+            if (i === index) {
+                setTimeout(() => {
+                    section.style.opacity = 1;
+                }, 250);
+                section.style.display = "flex";
+            } else {
+                section.style.opacity = 0;
+                setTimeout(() => {
+                    section.style.display = "none";
+                }, 1000);
+            }
+        });
     }
+
+    function nextSection() {
+        currentSection = (currentSection + 1) % sections.length;
+        showSection(currentSection);
+    }
+
+    showSection(currentSection);
+    setInterval(nextSection, 10000);
 });
